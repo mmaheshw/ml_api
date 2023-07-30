b5f613277bf395a96e6e9fb4e74d2f1c2060fead
Manjari <mmaheshwari@stclaircollege.ca>
2023-07-30 12:40:14 -0400

    Changes

--- ml/tests/test_project.py
+++ ml/tests/test_project.py
@@ -65,3 +65,71 @@ def test_get():
     assert r.status_code == 200
     assert r.json() == "The API is working!"
 
+def test_post_50k(data, encoder_lb):
+    encoder, _ = encoder_lb
+
+    """
+    # take a sample from the data with a high salary
+    sample=data[data['salary']=='<=50K'].iloc[500].to_dict()
+    # remove target
+    _ = sample.pop('salary')
+    """
+   
+    sample = {
+        "age": 50,
+        "workclass": "Private",
+        "fnlgt": 287372,
+        "education": "Doctorate",
+        "education_num": 16,
+        "marital_status": "Husband",
+        "occupation": "Exec-managerial",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Male",
+        "capital_gain": 14084,
+        "capital_loss": 0,
+        "hours_per_week": 50,
+        "native_country": "United-States"
+        }
+    #data = json.dumps(sample)
+    #print(data)
+
+    r = client.post("/inference/", json=sample )
+    
+
+     # test response and output
+    assert r.status_code == 200
+    assert r.json()["prediction"]== '>50K'
+
+def test_post_0k(data, encoder_lb):
+    encoder, _ = encoder_lb
+
+    """
+    # take a sample from the data with a high salary
+    sample=data[data['salary']=='>50K'].iloc[500].to_dict()
+    # remove target
+    _ = sample.pop('salary')
+
+    data = json.dumps(sample)
+    """
+    sample =  {  'age':24,
+                'workclass':"Private", 
+                'fnlgt':505119,
+                'education':"Some-college",
+                'education_num':14,
+                'marital_status':"Married-civ-spouse",
+                'occupation':"Sales",
+                'relationship':"Husband",
+                'race':"Black",
+                'sex':"Male",
+                'capital_gain':0,
+                'capital_loss':0,
+                'hours_per_week':0,
+                'native_country':"Cuba"
+            }
+
+    r = client.post("/inference/", json=sample )
+
+     # test response and output
+    assert r.status_code == 200
+ 
